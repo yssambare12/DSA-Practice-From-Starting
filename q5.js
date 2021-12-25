f7685f530a9e8f6ef095535ac83bd9f2511452a1
Yogesh  Sambare <bloggingscan@gmail.com>
2021-12-25 23:17:30 +0530

    Update q5.js

--- q5.js
+++ q5.js
@@ -5,3 +5,18 @@
 // The value of the alphabetais1,b is 2,c is 3..... tillz is 26.
 
 // Now you have to find the total value of the given string. The total value of a string is the sum of values of all characters present in string.
+
+
+function stringValue(S) {
+    //write code here
+   var string = "abcdefghijklmnopqrstuvwxyz";
+   var count = 0;
+      for (var i = 0; i < S.length; i++){
+          for (var j = 1; j < S.length; j++){
+              if(S[i] == string[j]){
+                  count = count + j;
+              }
+          }
+      }
+      console.log(count)
+  }
