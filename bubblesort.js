6fed9ba68ea43485338e55a428deb7e9cd5d8196
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2022-01-30 21:49:28 +0530

    Update bubblesort.js

--- bubblesort.js
+++ bubblesort.js
@@ -11,3 +11,43 @@
 // Sample Output 1
 
 // 0 3 5 8 9
+
+
+
+function bubblesort(arr){
+    for (var i = 1; i <arr.length; i++){
+        for (var j = 0; j <arr.length - i; j++){
+            if (arr[j] > arr[j+1]){
+                [arr[j], arr[j+1]] = [arr[j+1],arr[j]]
+            }
+        }
+    }
+    console.log(arr.join(" "))
+}
+
+function runProgram(input){
+    input = input.split("\n")
+    var arr = input[1].split(" ").map(Number);
+    bubblesort(arr);
+}
+
+if (process.env.USERNAME === "Admin") {
+    runProgram(input);
+} else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function(input) {
+        read += input;
+    });
+    process.stdin.on("end", function() {
+        read = read.replace(/\n$/, "");
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+    });
+    process.on("SIGINT", function() {
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+        process.exit(0);
+    });
+}
