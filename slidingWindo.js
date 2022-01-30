84a2eceb5e631afba0e82aa45cf7d321c5a4fea4
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2022-01-30 21:52:35 +0530

    Create slidingWindo.js

---
+++ slidingWindo.js
@@ -0,0 +1,10 @@
+// Description
+
+// Given an array of integers and a number k, find the maximum sum of a subarray of size k.
+// Sample Input 1 
+
+// 10 3
+// -1 -1 -2 1 -2 4 1 9 3 9
+// Sample Output 1
+
+// 21
